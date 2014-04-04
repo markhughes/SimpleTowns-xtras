3395e08a9ffcfcc3112b60b43f949c54831f295a
MarkehMe <mark@markeh.me>
2014-04-05 04:50:07 +1100

    Adding unregister events and support for ChunkVisualizer

--- src/markehme/simpletownsxtras/SimpleTownsXtrasListeners.java
+++ src/markehme/simpletownsxtras/SimpleTownsXtrasListeners.java
@@ -6,10 +6,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 
+import com.gmail.jameshealey1994.simpletowns.events.TownClaimEvent;
+import com.gmail.jameshealey1994.simpletowns.events.TownCreateEvent;
+
 public class SimpleTownsXtrasListeners {
 
 	private static PluginManager pm;
-	
 	
 	/**
 	 * Sets up event listeners that are required
@@ -24,13 +26,22 @@ public class SimpleTownsXtrasListeners {
 				register(new WorldGuardListener());
 			}
 		}
+		
+		// If ChunkVisualizer is enabled, then enable our ChunkVisualizer features
+		if(pm.getPlugin("ChunkVisualizer") != null) {
+			if(pm.getPlugin("ChunkVisualizer").isEnabled()) {
+				register(new ChunkVisualizerListener());
+			}
+		}
+		
 	}
 	
 	/**
 	 * Removes events we registered 
 	 */
 	public void deRegister() {
-		
+		TownClaimEvent.getHandlerList().unregister(SimpleTownsXtras.instance); 
+		TownCreateEvent.getHandlerList().unregister(SimpleTownsXtras.instance); 
 	}
 	
 	/** 
