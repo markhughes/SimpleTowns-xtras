7ba598bbcd329111fa03d9828a872f5406cbd1f9
MarkehMe <mark@markeh.me>
2014-04-08 15:31:29 +1000

    Testing and working explosions listener

--- src/markehme/simpletownsxtras/listeners/ExplosionListener.java
+++ src/markehme/simpletownsxtras/listeners/ExplosionListener.java
@@ -1,10 +1,14 @@
 package markehme.simpletownsxtras.listeners;
 
 
+import java.util.ArrayList;
+
 import markehme.simpletownsxtras.SimpleTownsXtras;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -18,36 +22,76 @@ import com.gmail.jameshealey1994.simpletowns.object.Town;
 
 public class ExplosionListener implements Listener {
 	
-	@EventHandler(priority=EventPriority.LOW)
+	@EventHandler(priority=EventPriority.LOWEST)
 	public void onPreExplosionEvent(ExplosionPrimeEvent e) {
 		Location l = e.getEntity().getLocation();
 		
 		String location = l.getWorld().getName() + " " + l.getChunk().getX() + " " + l.getChunk().getZ();
 		
+		// Add this information for later use
 		e.getEntity().setMetadata("SimpleTownsXtras-primed", new FixedMetadataValue(SimpleTownsXtras.instance, location));
 		
 	}
 	
-	@EventHandler(priority=EventPriority.LOW)
+	@EventHandler(priority=EventPriority.HIGH)
 	public void onEntityExplode(EntityExplodeEvent e) {
-				
+		
+		// Ensure there a blocks being blown up
+		if(!(e.blockList().size() > 0)) {
+			return;
+		}
+		
+		// Creepers in Towns Block
+		if(SimpleTownsXtras.config.getBoolean("explosions.disallowCreepersInTowns") && e.getEntity().getType() == EntityType.CREEPER) {
+			Town townCreeperIn = SimpleTownsXtras.simpleTowns.getTown(e.getEntity().getLocation().getChunk());
+			
+			// If we're not in the wilderness, clear all blocks
+			if(townCreeperIn != null) {
+				e.blockList().clear();
+			}
+			
+			return;
+		}
+		
 		// TNT Fix 
-		if(e.getEntity().getType() == EntityType.PRIMED_TNT && e.blockList().size() > 0){
-				
+		if(SimpleTownsXtras.config.getBoolean("explosions.disallowTNTIfFromOutsideTown") && e.getEntity().getType() == EntityType.PRIMED_TNT){
+			
+			SimpleTownsXtras.log(e.getEntity().getMetadata("SimpleTownsXtras-primed").get(0).asString());
 			// Grab the meta data we set in ExplosionPrimeEvent
 			String[] metaData = e.getEntity().getMetadata("SimpleTownsXtras-primed").get(0).asString().split(" ");
 			
+			World worldIn = Bukkit.getWorld(metaData[0]);
+			
+			Chunk chunkIn = worldIn.getChunkAt(Integer.valueOf(metaData[1]), Integer.valueOf(metaData[2]));
+			
 			// Get town that it was primed in
-			Town townPrimedIn = SimpleTownsXtras.simpleTowns.getTown(Bukkit.getWorld(metaData[0]).getChunkAt(Integer.valueOf(metaData[1]), Integer.valueOf(metaData[2])));
-
+			Town townPrimedIn = SimpleTownsXtras.simpleTowns.getTown(chunkIn);
+			
+			String primedIn;
+			if(townPrimedIn == null) {
+				primedIn = "notown";
+			} else {
+				primedIn = townPrimedIn.getName();
+			}
+			
+			SimpleTownsXtras.log("Primed in " + primedIn);
+			
+			// work with a copy 
+			ArrayList<Block> listOfBlocks = new ArrayList<Block>(e.blockList());
+			
 			// Loop through each block thats being destroyed and see if it's in a Town
-			for(Block block : e.blockList()){
+			for(Block block : listOfBlocks) {
 				Town currentTown = SimpleTownsXtras.simpleTowns.getTown(block.getChunk());
 				
 				// If no town, continue explosion
-				if(currentTown == null) break;
+				if(currentTown == null)  {
+					SimpleTownsXtras.log("Block in notown - no change");
+					break;
+				}
 				
-				// If not in same town as where to exploded, then remove
+				SimpleTownsXtras.log("Block in " + currentTown.getName());
+				
+				// If the town names don't match, don't break the block
 				if(townPrimedIn.getName() != currentTown.getName()) {
 					e.blockList().remove(block);
 				}
